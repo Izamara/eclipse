dd6810685e30b6b1117f31799ae777773b60b4f8
Izamara <58221086+Izamara@users.noreply.github.com>
2019-11-28 13:41:01 -0300

    Add files via upload

---
+++ Projeto GitHub/src/br/com/izamara/git/ProjetoGitHub.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.izamara.git;
+
+/**
+ * @author 20161204010019
+ * Integração do Eclipse com o GitHub
+ */
+public class ProjetoGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Projeto");
+
+	}
+
+}
